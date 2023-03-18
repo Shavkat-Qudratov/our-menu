36af1602c85511dbc43f4ac869ca1ffb544c34f6
shavkatbek11 <shavkatbekq69@gmail.com>
2023-03-18 13:16:08 +0500

    commit

---
+++ src/data.js
@@ -0,0 +1,75 @@
+const menu = [
+	{
+		id: 1,
+		title: 'buttermilk pancakes',
+		category: 'breakfast',
+		price: 15.99,
+		img: 'https://tornadoughalli.com/wp-content/uploads/2021/12/BUTTERMILK-PANCAKE-RECIPE-3-1.jpg',
+		desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `
+	},
+	{
+		id: 2,
+		title: 'diner double',
+		category: 'lunch',
+		price: 13.99,
+		img: 'https://www.shutterstock.com/image-photo/iconic-diner-double-cheeseburger-stacked-260nw-694423843.jpg',
+		desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `
+	},
+	{
+		id: 3,
+		title: 'godzilla milkshake',
+		category: 'shakes',
+		price: 6.99,
+		img: 'https://i2-prod.glasgowlive.co.uk/incoming/article16335485.ece/ALTERNATES/s810/0_DSC_5315.jpg',
+		desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`
+	},
+	{
+		id: 4,
+		title: 'country delight',
+		category: 'breakfast',
+		price: 20.99,
+		img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS4y_MZuptorKjyO0C1ClUBkkaaBXR2XMqFA&usqp=CAU',
+		desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `
+	},
+	{
+		id: 5,
+		title: 'egg attack',
+		category: 'lunch',
+		price: 22.99,
+		img: 'https://thumbor.forbes.com/thumbor/fit-in/x/https://www.forbes.com/health/wp-content/uploads/2022/08/workday_lunch_ideas.jpeg.jpg',
+		desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `
+	},
+	{
+		id: 6,
+		title: 'oreo dream',
+		category: 'shakes',
+		price: 18.99,
+		img: 'https://img.sndimg.com/food/image/upload/q_92,fl_progressive,w_1200,c_scale/v1/img/recipes/48/59/23/pic14RvB2.jpg',
+		desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`
+	},
+	{
+		id: 7,
+		title: 'bacon overflow',
+		category: 'breakfast',
+		price: 8.99,
+		img: 'https://www.menu.anneson.com/images/item-7.jpeg',
+		desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `
+	},
+	{
+		id: 8,
+		title: 'american classic',
+		category: 'lunch',
+		price: 12.99,
+		img: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/all-american-t-bone-0474582.jpg?quality=90&resize=440,400',
+		desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `
+	},
+	{
+		id: 9,
+		title: 'quarantine buddy',
+		category: 'shakes',
+		price: 16.99,
+		img: 'https://c1.staticflickr.com/9/8509/8573292854_e281c98b6a_b.jpg',
+		desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`
+	}
+];
+export default menu;
